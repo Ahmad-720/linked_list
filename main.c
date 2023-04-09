42870a3ee1983dabe1a8cc42858e23dbfee18f90
Ahmad <ahmad_chebbo1@hotmail.com>
2023-04-09 16:16:35 +0300

    created a function to create a linked list

--- main.c
+++ main.c
@@ -8,3 +8,32 @@ struct node{
 
 typedef struct node node;
 
+node *create_list(){
+    int n;
+    do{
+        printf("Enter how many nodes you want: ");
+        scanf("%d",&n);
+    }while(n<0);
+    if(n == 0) return NULL;
+    node *head = (node *)malloc(sizeof(node));
+    node *ptr = head;
+    node *pre;
+    for(int i = 0;i<n;i++){
+        printf("ENter data: ");
+        scanf("%d",&ptr->data);
+        ptr->link = (node *)malloc(sizeof(node));
+        pre = ptr;
+        ptr = ptr->link;
+    }
+    pre->link = NULL;
+    return head;
+}
+
+void print_list(node *head){
+    while(head != NULL){
+        printf("%d",head->data);
+        if(head->link != NULL) printf(" -> ");
+        head = head->link;
+    }
+    printf("\n");
+}
